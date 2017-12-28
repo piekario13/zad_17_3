d80f587407df328a99bbf9b9f66d3bda7b9839df
piekario13 <piekario13@gmail.com>
2017-12-28 19:56:19 +0100

    zad_17_4

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var fs = require('fs');
-var stringifyFile;
-
-app.use(bodyParser.json());
-
-app.get('/getNote', function(req, res){
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data
-        res.send(data);
-    });
+app.use(express.static('assets'));
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
 });
 
-app.post('/updateNote/:note', function(req, res) {
-	stringifyFile = req.params.note;
-	fs.writeFile('./test.json', stringifyFile, function(err) {
-		if(err) throw err;
-		console.log('file updated');
-	});
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
 
-app.listen(3000);
-
-
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
